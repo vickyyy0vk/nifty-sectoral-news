b14b45d5f18bf61b932ea7b7b83d0058f915414d
vickyyy0vk <160882757+vickyyy0vk@users.noreply.github.com>
2025-08-03 08:55:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// All dynamic data is now handled by nse_data_updater.js!
-// Main script file
+// Nifty Sectoral News Hub - Main Script
 // All dynamic content is handled by nse_data_updater.js
 
 console.log('📊 Nifty Sectoral News Hub - Main Script Loaded');
